cd6ea2b9674d558c6d74d9aea13d45250cd87e74
Robert Jones <robert.jones@finaps.nl>
2022-06-22 10:59:00 +0200

    initialise nx with angular app

---
+++ finaps/apps/mfing-website/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'finaps-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'mfing-website';
+}
