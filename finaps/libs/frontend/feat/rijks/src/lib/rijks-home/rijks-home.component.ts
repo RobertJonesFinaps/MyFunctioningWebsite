13ae61811e6479d637cd209bcc622956beee0517
Hamoud <hamoud.khamis@finaps.nl>
2022-06-30 15:09:09 +0200

    Merge branch 'rijk' of /Users/hamoudkhamis/MyFunctioningWebsite with conflicts.

--- finaps/libs/frontend/feat/rijks/src/lib/rijks-home/rijks-home.component.ts
+++ finaps/libs/frontend/feat/rijks/src/lib/rijks-home/rijks-home.component.ts
@@ -10,23 +10,26 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class RijksHomeComponent implements OnInit {
   searchForm!: FormGroup;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
+
 
   ngOnInit(): void {
     this.searchForm = new FormGroup({
-      searchTerm: new FormControl(null),
-    });
+      searchTerm: new FormControl(null)
+    })
 
-    this.http
-      .get<any>('http://localhost:3333/api/rijks/collection', {
-        params: { culture: 'en', format: 'json' },
-      })
-      .subscribe((response: any) => {
-        console.table(response);
-      });
-  }
+    this.http.get('http://localhost:3333/api/rijks/collection',{
+      "format": "json",
+      "culture": "en",
+      "p": 1,
+      "ps": 10
+      } 
+   ).subscribe((response: any) => {
+      console.table(response)
+    })
+}
 
   onSubmit(): void {
-    console.log(this.searchForm);
+    console.log(this.searchForm)
   }
 }
