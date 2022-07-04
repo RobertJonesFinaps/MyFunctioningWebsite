cad8bcd43ec21dc65cb102c72f54cd2af8c7c0c7
Hamoud <hamoud.khamis@finaps.nl>
2022-07-04 13:30:48 +0200

    configured search by keyword

--- finaps/libs/frontend/feat/rijks/src/lib/rijks-home/rijks-home.component.ts
+++ finaps/libs/frontend/feat/rijks/src/lib/rijks-home/rijks-home.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Collection } from './rijks-home.interface';
+import { map} from 'rxjs';
+import { Collection, Image } from './rijks-home.interface';
 
 @Component({
   selector: 'finaps-rijks-home',
@@ -10,24 +11,47 @@ import { Collection } from './rijks-home.interface';
 })
 export class RijksHomeComponent implements OnInit {
   searchForm!: FormGroup;
+  imageTitles: string[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.searchForm = new FormGroup({
       searchTerm: new FormControl(null),
     });
-
-
   }
 
   searchWholeCollection() {
-    this.http.get<Collection>('http://localhost:3333/api/rijks/collection', {
-      params: { culture: 'en', format: 'json' },
-    }
-    ).subscribe((response) => {
-      console.table(response.artObjects)
+    this.http
+      .get<Collection>('http://localhost:3333/api/rijks/collection', {
+        params: { culture: 'en', format: 'json' },
+      })
+      .pipe(
+        map((collection) => {
+          return collection.artObjects.map((artObject) => artObject.title);
+        })
+      )
+      .subscribe((imageTitles) => {
+        this.imageTitles = imageTitles;
+      });
+  }
+
+  searchByKeyword(){
+    const searchTerm = this.searchForm.get('searchTerm')?.value
+    this.http
+    .get<Collection>('http://localhost:3333/api/rijks/collection', {
+      params: { culture: 'en', format: 'json' , q: searchTerm},
     })
+    .pipe(
+      map((collection) => {
+       return collection.artObjects.map((artObject) => artObject.title);
+      })
+    )
+    .subscribe((imageTitles) => {
+      this.imageTitles = imageTitles;
+    });
+
+    this.searchForm.reset()
   }
 
   onSubmit(): void {
